e3189aee51ac34c75e480ee1600c8b34e2c4954c
Felício Almeida <feliciorar@gmail.com>
2023-10-10 15:26:17 -0300

    Update ex12.c

--- PROG 2/Lista 5/ex12.c
+++ PROG 2/Lista 5/ex12.c
@@ -8,31 +8,47 @@
       ********************************************************************/
 
       /* Importação de Bibliotecas */
-      #include <stdio.h>
+#include <stdio.h>
 
-      /* Declaração de constantes */
+/* Declaração de constantes */
+#define TAM_GABARITO 13
+#define TAM_RESPOSTAS 13
+#define APOSTADORES 10
 
-      /* Declaração de estruturas
-         Nome:
-         Objetivo: */
 
-      /* Declaração de funções
-         Nome:
-         Entrada:
-         Saída:
-         Objetivo: */
+/* Função principal */
+int main() {
+    /* Declaração de variáveis */
+    int g[TAM_GABARITO], r[TAM_RESPOSTAS];
+    int j, i, acertos;
 
-      /* Função principal */
-      int main() {
-         /* Declaração de variáveis */
+    //Entrada -
+    printf("Digite os valores do gabarito: \n");
+    for (i = 0; i < TAM_GABARITO; i++){
+        printf("Valor[%d]:", i+1);
+        scanf("%d", &g[i]);
+    }
 
-         //Entrada - A substituir com comentário adequado
 
-         //Processamento - A substituir com comentário adequado
+    //Processamento -
+    for (i = 0; i < APOSTADORES; i++){
+        acertos = 0;
+        printf("Digite as respostas do apostador [%d]:\n", i+1);
+        for (j = 0; j< TAM_RESPOSTAS; j++) {
+            printf("Resposta[%d]:", j+1);
+            scanf("%d", &r[j]);
+            if (r[j] == g[i]){
+                acertos++;
+            }
+        }
+        printf("Acertos do apostador %d : %d\n", i+1, acertos);
+    }
 
-         //Saída - A substituir com comentário adequado
 
-         //Encerramento
-         getchar();
-         return 0;
-      }
+    //Saída -
+
+
+    //Encerramento
+    getchar();
+    return 0;
+}
