7c9033fc5b4b74e4bdc9d32b4026de9b77f50431
Felício Almeida <feliciorar@gmail.com>
2023-11-09 11:03:08 -0300

    Add files via upload

---
+++ PROG 2/Lista 8/Lista 8 - ex1.c
@@ -0,0 +1,40 @@
+/***************************************************************************
+   CABEÇALHO
+   Enunciado:
+   Autor: Felicio Almeida
+
+   Histórico de Versão
+   1.0 Versão Original. Responsável: Felicio Almeida
+********************************************************************/
+
+/* Importação de Bibliotecas */
+#include <stdio.h>
+
+
+float cubo (float numero) {
+    float cuboNumero;
+
+    cuboNumero = numero*numero*numero;
+
+    return cuboNumero;
+}
+
+/* Função principal */
+int main() {
+    /* Declaração de variáveis */
+    float num;
+
+    //Entrada -
+    printf("Digite um valor para conferir seu cubo: ");
+    scanf("%f", &num);
+
+    //Processamento -
+    num = cubo(num);
+
+    //Saída -
+    printf("O cubo do numero: %f", num);
+
+    //Encerramento
+    getchar();
+    return 0;
+}
