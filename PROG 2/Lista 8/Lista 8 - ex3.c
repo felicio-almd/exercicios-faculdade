b9e1d89c5131e76584cb20bc34ebf3cd3c1306d4
Felício Almeida <feliciorar@gmail.com>
2023-11-16 10:11:45 -0300

    Add files via upload

---
+++ PROG 2/Lista 8/Lista 8 - ex3.c
@@ -0,0 +1,41 @@
+/***************************************************************************
+   CABEÇALHO
+   Enunciado:
+   Autor: Felicio Almeida
+
+   Histórico de Versão
+   1.0 Versão Original. Responsável: Felicio Almeida
+********************************************************************/
+
+/* Importação de Bibliotecas */
+#include <stdio.h>
+
+
+void sinal (int n) {
+    if (n > 0) {
+        printf("Numero positivo.\n");
+    }
+    else if (n<0){
+        printf("Numero negativo.\n");
+    }
+    else {
+        printf("Numero igual a zero.\n");
+    }
+}
+
+/* Função principal */
+int main() {
+    /* Declaração de variáveis */
+    int num;
+
+    //Entrada -
+    printf("Digite um valor de N: ");
+    scanf("%d", &num);
+
+    //Processamento e saida -
+    sinal(num);
+
+    //Encerramento
+    getchar();
+    return 0;
+}
