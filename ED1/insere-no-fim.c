487ee3d839cb159a999081e3d461a8103697b1ce
Felicio <feliciorar@gmail.com>
2024-07-22 23:54:14 -0300

    refatoração

--- ED1/insere-no-fim.c
+++ ED1/insere-no-fim.c
@@ -1,50 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct no {
+typedef struct no
+{
     int valor;
-    struc no *prox;
+    struct no *prox;
 } No;
 
 // recebe um ponteiro para ponteiro, porque teremos um ponteiro pra lista no metodo, entao recebe o endereço
-void inserirNoInicio (No **lista, int num) {
+void inserirNoInicio(No **lista, int num)
+{
     No *novo = malloc(sizeof(No));
 
-    if(novo) {
+    if (novo)
+    {
         novo->valor = num;
         novo->prox = *lista;
         *lista = novo;
     }
-    else {
+    else
+    {
         printf("Erro ao alocar memoria");
     }
 }
 
 // procedimento para inserir no fim
-void insereNoFim (No **lista, int num) {
+void insereNoFim(No **lista, int num)
+{
     No *novo = malloc(sizeof(No));
     No *aux = malloc(sizeof(No));
 
-    if (novo) {
+    if (novo)
+    {
         novo->valor = num;
         novo->prox = NULL;
         // eh o primeiro?
-        if ( *lista == NULL ){
+        if (*lista == NULL)
+        {
             *lista = novo;
-        } else {
+        }
+        else
+        {
             aux = *lista;
-            while (aux->prox != NULL) {
+            while (aux->prox != NULL)
+            {
                 aux = aux->prox;
             }
             aux->prox = novo;
         }
     }
-    else {
+    else
+    {
         printf("Erro ao alocar");
     }
 }
 
-int main () {
+int main()
+{
 
     return 0;
 }
