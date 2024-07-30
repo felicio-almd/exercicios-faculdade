3ec0cacfdcccc1ff422609be5420377f1d57c425
Felicio <feliciorar@gmail.com>
2024-07-30 17:05:21 -0300

    add lista tipo generico de dado

---
+++ ED1/listaTipoGenerico.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+// Implementar uma lista simplesmente encadeada para tipo genérico de dado
+
+typedef struct listagen
+{
+    void *info;
+    struct listagen *prox;
+} ListaGen;
+
+int main()
+{
+    return 0;
+}
