f99c0578e06e3151acdf4ae1a1e24b16f87629d8
Felicio <feliciorar@gmail.com>
2024-08-05 11:35:09 -0300

    add questão 6

--- ED1/LISTA P1/Q6-lista-com-sentinela.c
+++ ED1/LISTA P1/Q6-lista-com-sentinela.c
@@ -112,11 +112,77 @@ void removeChave(Lista *L, int valor)
         }
     }
 }
+
 // e) Recebe duas listas, L1 e L2, e duas chaves, c1 e c2. Busca c1 na lista L1, busca c2 em
 // L2. Caso as duas chaves existam nas respectivas listas, realiza a troca das chaves entre
 // as listas. Ou seja, retira os nós que contém c1 e c2 de L1 e L2, respectivamente. Insere
 // c1 em L2 na posição onde estava c2. Insere c2 em L1 na posição onde estava c1. Sua
 // função deve ter complexidade linear.
+No *buscaNo(Lista *L, int valor)
+{
+    No *aux = L->inicio;
+    if (L == NULL) // O(1)
+    {
+        return NULL;
+    }
+    while (aux != NULL) // O(n)
+    {
+        if (aux->ch == valor)
+        {
+            return aux;
+        }
+        aux = aux->prox;
+    }
+    printf("No nao encontrado"); // O(1)
+    return NULL;
+}
+
+void trocaNo(Lista *L1, Lista *L2, int c1, int c2)
+{
+    No *aux1 = NULL;
+    No *aux2 = NULL;
+    No *tempAnt = NULL;
+    No *tempProx = NULL;
+    if (L1 == NULL || L2 == NULL) // O(1)
+    {
+        printf("Uma das listas é vazia");
+        return;
+    }
+    aux1 = buscaNo(L1, c1);           // O(n)
+    aux2 = buscaNo(L2, c2);           // O(n)
+    if (aux1 == NULL || aux2 == NULL) // O(1)
+    {
+        printf("Uma das lista nao tem a chave desejado");
+        return;
+    }
+
+    if (aux1->ant != NULL) // O(1)
+        aux1->ant->prox = aux2;
+    else // O(1)
+        L1->inicio = aux2;
+    if (aux1->prox != NULL) // O(1)
+        aux1->prox->ant = aux2;
+    else // O(1)
+        L1->fim = aux2;
+
+    if (aux2->ant != NULL) // O(1)
+        aux2->ant->prox = aux1;
+    else // O(1)
+        L2->inicio = aux1;
+    if (aux2->prox != NULL) // O(1)
+        aux2->prox->ant = aux1;
+    else // O(1)
+        L2->fim = aux1;
+
+    tempAnt = aux1->ant;
+    tempProx = aux1->prox;
+    aux1->ant = aux2->ant;
+    aux1->prox = aux2->prox;
+    aux2->ant = tempAnt;
+    aux2->prox = tempProx;
+}
+
+// Resultado: 0(1) + O(n) = O(n), a função tem complexidade O(n) linear, onde n é o numero de nós das listas combinadas.
 
 // f) Calcule a função de complexidade de tempo da função do item anterior. Conclua com
 // a notação assintótica da complexidade calculada.
